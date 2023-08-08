682a65ff25526c634f2b9add584df2f854765a79
root <sandramabonga303@gmail.com>
2023-08-08 15:50:18 -0700

    romove additional print function

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    if i in range(0, 10):
-        print("0{}, ".format(i), end='')
-    elif i != 99:
-        print("{}, ".format(i), end='')
+    if i != 99:
+        print("{:02}, ".format(i), end='')
     else:
-        print(i)
+        print("{}".format(i))
