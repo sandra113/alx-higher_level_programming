45de6d8b4517a105a8ee65c42967e88f6950f1c1
root <sandramabonga303@gmail.com>
2023-08-08 05:51:09 -0700

    use print with string format

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for val in range(97, 123):
-    print(chr(val), end='')
+str = ''.join(chr(val) for val in range(97, 123))
+print(str, end='')
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
-for val in range(97, 123):
-    if chr(val) not in ('e', 'q'):
-        print(chr(val), end='')
+str = ''.join(chr(val) for val in range(97,123) if chr(val) not in ('e', 'q'))
+print(str, end='')
