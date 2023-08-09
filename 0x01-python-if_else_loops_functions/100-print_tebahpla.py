9e781870fc50801ec5b9dcaa12c3a6907952e254
root <sandramabonga303@gmail.com>
2023-08-09 12:23:34 -0700

    alphabets

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+val = ""
+
+for i in range(97, 123):
+    val += chr(i)
+    val_len = len(val)
+
+for j in range(val_len):
+    if j % 2 == 1:
+        if 'a' <= val[j] <= 'z':
+            upper = ord(val[j]) - ord('a') + ord('A')
+            val = val[:j] + chr(upper) + val[j+1:]
+print(val)
