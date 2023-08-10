9d8fba285dd6d72403a974897714eef97e8ee691
sandra113 <sandramabonga303@gmail.com>
2023-08-10 23:45:58 +0300

    calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-import calculator_1
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
-sum = calculator_1.add(a, b)
-dif = calculator_1.sub(a, b)
-mult = calculator_1.mul(a, b)
-dv = calculator_1.div(a, b)
+    a = 10
+    b = 5
+    sum = add(a, b)
+    dif = sub(a, b)
+    mult = mul(a, b)
+    dv = div(a, b)
 
 print("{0} + {1} = {2}".format(a, b, sum))
 print("{0} - {1} = {2}".format(a, b, dif))
