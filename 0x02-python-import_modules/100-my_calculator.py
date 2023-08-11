498c656ed5acf9c14e03cd9486d50710b27ebf61
root <sandramabonga303@gmail.com>
2023-08-11 06:01:11 -0700

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
+    args = sys.argv[1:]
+    arg_len = len(args)
+
+    if arg_len != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(args[0])
+    b = int(args[2])
+
+    if args[1] == '+':
+        t_sum = add(a, b)
+        print("{} + {} = {}".format(a, b, t_sum))
+    elif args[1] == '-':
+        dif = sub(a, b)
+        print("{} + {} = {}".format(a, b, dif))
+    elif args[1] == '*':
+        product = mul(a, b)
+        print("{} + {} = {}".format(a, b, product))
+    elif args[1] == '/':
+        dv = div(a, b)
+        print("{} + {} = {}".format(a, b, dv))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from hidden_4.pyc import *
+    from hidden_4.pyc import names 
 
     print()
