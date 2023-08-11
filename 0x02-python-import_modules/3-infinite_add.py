99964b0d4a61bff9bceb5d93f3c9d0ebd9d3b7ae
root <sandramabonga303@gmail.com>
2023-08-11 04:48:32 -0700

    adding infinite numbers

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    
+    args = sys.argv[1:]
+    arg_len = len(args)
+
+    total_sum = 0
+
+    for i in range(arg_len):
+        num = int(args[i])
+        total_sum += num
+print("{}".format(total_sum))
