a996ca00545775f6c16bd5c0fc0c05bf54d0a65f
root <sandramabonga303@gmail.com>
2023-08-11 04:21:37 -0700

    handling import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-args = sys.argv[1:]
+    args = sys.argv[1:]
+    args_num = len(args)
 
-args_num = len(args)
+    if args_num == 0:
+        print("{} arguments.".format(args_num))
+    elif args_num == 1:
+        print("{} argument:".format(args_num))
+    else:
+        print("{} arguments:".format(args_num))
 
-if args_num == 0:
-    print("{} arguments.".format(args_num))
-elif args_num == 1:
-    print("{} argument:".format(args_num))
-else:
-    print("{} arguments:".format(args_num))
-
-for i in range(args_num):
-    print("{}: {}".format(i + 1, args[i]))
+    for i in range(args_num):
+        print("{}: {}".format(i + 1, args[i]))
