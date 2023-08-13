4162c2fa58bea18026e92806368f0276b2ee1beb
root <sandramabonga303@gmail.com>
2023-08-13 08:17:31 -0700

    reverse a loop

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    length = len(my_list)
+
+    new_list = my_list[length-1::-1]
+
+    for  i in range(length):
+
+        print(new_list[i])
