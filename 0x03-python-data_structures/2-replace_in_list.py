ab796ac0874c6d142cd83967771cd21cd741c34f
root <sandramabonga303@gmail.com>
2023-08-13 08:48:47 -0700

    removed iteration

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,8 +5,5 @@ def replace_in_list(my_list, idx, element):
 
     if idx < 0 or idx > (length - 1):
         return(my_list)
-
-    for i in range(length):
-        if i == idx:
-            my_list[i] = element
+    my_list[idx] = element
     return(my_list)
