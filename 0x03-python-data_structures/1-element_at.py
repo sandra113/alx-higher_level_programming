686159959f4619b85f6b7167057e0172d7eea95a
root <sandramabonga303@gmail.com>
2023-08-13 07:15:51 -0700

    elements in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,6 +4,8 @@ def element_at(my_list, idx):
     if idx < 0:
         return(None)
     length = len(my_list)
+    if idx > length:
+        return(None)
     for i in range(length):
         if i == idx:
             return(my_list[i])
