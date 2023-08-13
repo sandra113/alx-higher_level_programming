7d38a7a97f7e51cc1b729aa2585bc5a76342f1c4
root <sandramabonga303@gmail.com>
2023-08-13 10:07:13 -0700

    return statement

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,10 +2,10 @@
 
 def element_at(my_list, idx):
     if idx < 0:
-        return(None)
+        return None
     length = len(my_list)
     if idx > length:
-        return(None)
+        return None
     for i in range(length):
         if i == idx:
-            return(my_list[i])
+            return my_list[i]
