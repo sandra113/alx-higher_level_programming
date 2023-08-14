eb87c52c7af36ba8dadf5d83f1cfd799a1666fd0
root <sandramabonga303@gmail.com>
2023-08-14 05:49:37 -0700

    correction

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,9 @@
+!bin/bash
+
+/**
+ * print_python_list_info - prints some basic python lists infor
+ * @PyObjece :
+ * @p:
+ */
+
+void print_python_list_info(PyObject *p)
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,6 +5,5 @@ def print_reversed_list_integer(my_list=[]):
 
     new_list = my_list[length-1::-1]
 
-    for i in range(length):
-
-        print("{:d}".format(new_list[i]))
+    for i in new_list:
+        print("{:d}".format(i))
