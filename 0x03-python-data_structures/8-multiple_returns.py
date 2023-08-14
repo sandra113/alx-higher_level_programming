c3a5e7199a1e00bd286feaac799939e72765a1e1
root <sandramabonga303@gmail.com>
2023-08-14 10:27:42 -0700

    check if sentence is empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = len(sentence)
-    first_char = sentence[0]
 
-    return length, first_char
+    if sentence:
+        length = len(sentence)
+        first_char = sentence[0]
+
+        return length, first_char
