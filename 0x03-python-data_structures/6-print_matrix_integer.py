4ea56fd8a5ae55a792a20ea731a0dcb68a7ab44a
root <sandramabonga303@gmail.com>
2023-08-14 10:21:30 -0700

    fixing requirements

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" "if num != row[-1]else"")
-        print(f"")
+    if matrix:
+        for row in matrix:
+            for num in row:
+                print("{:d}".format(num), end=" "if num != row[-1]else"")
+            print()
