6661f63bffa6a660fdc2e496a550772630a0d94a
root <sandramabonga303@gmail.com>
2023-08-16 09:15:06 -0700

    common number

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
+    new_list = []
     for i in set_1:
         for k in set_2:
             if i == k:
-                return i
+                new_list.append(i)
+    return new_list
