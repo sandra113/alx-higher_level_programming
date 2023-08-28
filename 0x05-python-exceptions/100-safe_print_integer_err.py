89e140f521400bc0e6b1cd7cdff31ea03bb209bb
root <sandramabonga303@gmail.com>
2023-08-28 12:56:07 -0700

    exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as error:
+    except Exception as error:
         print("Exception:", error, file=sys.stderr)
         return False
