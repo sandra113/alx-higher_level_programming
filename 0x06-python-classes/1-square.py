b1d8423064b738b55bab6f6a34301c5034c74898
root <sandramabonga303@gmail.com>
2023-08-29 16:26:45 -0700

    creating an instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """This class represents a square."""
+
+    def __init__(self, size):
+        """
+        Initializes a square instance.
+
+        Args:
+            size(int): The size of the square.
+        """
+        self.__size = size
