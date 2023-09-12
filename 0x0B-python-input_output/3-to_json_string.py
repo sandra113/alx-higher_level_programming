eb413db529a626a8d19c5dce7fbda1ad4bb82c3a
root <sandramabonga303@gmail.com>
2023-09-12 04:43:31 -0700

    append to a file

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import json
 """JSON representation of a string"""
+import json
 
 
 def to_json_string(my_obj):
---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+
+
+def from_json_string(my_str):
