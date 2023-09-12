cf405b4dec3a9f5d35c7a1a42b220ef64ca4bea1
root <sandramabonga303@gmail.com>
2023-09-12 02:10:31 -0700

    new instance method area

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Defines an empty class BassGeometry"""
 
+
 class BaseGeometry:
     """Defines the class"""
     pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines an empty class BassGeometry"""
+
+
+class BaseGeometry:
+    """Defines the class"""
+    pass
+
+    def area(self):
+        """defining public instance method area"""
+        raise Exception("area() is not implemented")
