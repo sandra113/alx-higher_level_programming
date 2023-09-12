160267ad969fa3789cea7c0dee4941e221889877
root <sandramabonga303@gmail.com>
2023-09-12 04:14:48 -0700

    write into a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,7 @@
 
 
 def write_file(filename="", text=""):
-    """writes a text into a file and returns the number of characters written"""
+    """writes into a file and returns the number of chars written"""
     with open(filename, 'w', encoding="utf-8") as f:
         char_written = f.write(text)
         return char_written
