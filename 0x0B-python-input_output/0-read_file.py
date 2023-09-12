3cb5f51002b24db77ad0c95a028afade007a2d58
root <sandramabonga303@gmail.com>
2023-09-12 03:34:35 -0700

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module creates a funtion that reads a text file and prints it"""
+
+
+def read_file(filename=""):
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
