ae860ce1643cdb75056e32aa84051020093a421b
root <sandramabonga303@gmail.com>
2023-09-13 14:44:25 -0700

    squaree

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A module for a class square that  inherits from rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Class square that imports from Rectangle"""
+
+    def __init__(self, size):
+        """Initializes the instance size"""
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Returns a string representation of the square."""
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
