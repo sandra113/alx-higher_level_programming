c40c431527267c57bc901dd30a0540e7a3d81e9f
root <sandramabonga303@gmail.com>
2023-10-17 15:05:24 -0700

    Table Description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 -- prints the full description of the table first_table
-
+SELECT column_name, data_type, character_maximum_length
+FROM information_schema.columns
+WHERE table_name = 'first_table' AND table_schema = 'hbtn_0c_0';
