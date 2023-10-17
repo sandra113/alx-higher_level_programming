e8ac4587e14dc57b1d2d855b1b99b386c30cb861
root <sandramabonga303@gmail.com>
2023-10-17 15:24:05 -0700

    Table Description

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,17 @@
 -- prints the full description of the table first_table
-SELECT *FROM information_schema.columns
-WHERE table_name = 'first_table' AND table_schema = 'hbtn_0c_0';
+SELECT
+    table_name AS 'Table',
+    CONCAT('CREATE TABLE `', table_name, '` (') AS 'Create Table'
+FROM information_schema.tables
+WHERE table_schema = 'hbtn_0c_0'
+AND table_name = 'first_table';
+
+SELECT
+    GROUP_CONCAT(
+        CONCAT('  `', column_name, '` ', column_type, ' ',
+        IF(column_default IS NOT NULL, 'DEFAULT ' + column_default, ''), ',')
+    ) AS 'Create Table'
+FROM information_schema.columns
+WHERE table_name = 'first_table' AND table_schema = 'hbtn_0c_0'
+GROUP BY table_name;
+
