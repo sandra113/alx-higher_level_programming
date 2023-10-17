40577592dca08764166755c390b6986de23d9175
root <sandramabonga303@gmail.com>
2023-10-17 15:38:41 -0700

    Full Creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- This script creates a full table
+CREATE second_table
+(
+	id INT
+	name VARCHAR(256)
+	score INT
+)
+VALUES (
+	id = 1, name = "John", score = 10
+	id = 2, name = "Alex", score = 3
+	id = 3, name = "Bob", score = 14
+	id = 4, name = "George", score = 8
+);
