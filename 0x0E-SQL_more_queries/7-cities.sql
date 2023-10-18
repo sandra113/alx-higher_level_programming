a6de111b54c431976b33f72833c480d095cc4a96
root <sandramabonga303@gmail.com>
2023-10-18 15:25:26 -0700

    foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- This script creates the table force_name
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+
+-- Switch to the 'hbtn_0d_usa' database
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities
+(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCE states(id)
+);
