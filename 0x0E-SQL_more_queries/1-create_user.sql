1d04904ba7178831c6fed4683525e8b4ae255eb9
root <sandramabonga303@gmail.com>
2023-10-18 14:01:31 -0700

    create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- this script creates MySQL server user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES on *.* TO 'user_0d_1'@'localhost';
