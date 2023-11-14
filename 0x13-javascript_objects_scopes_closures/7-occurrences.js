31a1e9ea4e30c9043d9089807cd5e3c5aa352d68
sandra113 <sandramabonga303@gmail.com>
2023-11-14 14:50:04 -0800

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-exports.nbOccurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   const count = list.reduce((accumulator, currentElement) => {
     if (currentElement === searchElement) {
       return accumulator + 1;
