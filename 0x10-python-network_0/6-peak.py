07adecefc97a3cec2b02f55bda645efc1bebf7b8
root <sandramabonga303@gmail.com>
2023-11-30 14:16:38 -0800

    peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+    """
+    Finde a peak element in a list of unsorted integers
+    """
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
