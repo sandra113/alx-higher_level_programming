52152b50d09faf399bb4532af5782490489b7ab8
root <sandramabonga303@gmail.com>
2023-12-01 17:38:28 -0800

    response header

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) < 2:
+        print("Usage: ./script.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        x_request_id = response.headers.get('X-Request-Id')
+        print(x_request_id)
+
+    except requests.exceptions.RequestException as e:
+        print("Error accessing the URL:", e)
