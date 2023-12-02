6f440fda046ca7dc28b55e0e13042a2019e34ed9
root <sandramabonga303@gmail.com>
2023-12-01 16:26:07 -0800

    Response header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,15 +5,12 @@ if __name__ == "__main__":
     import urllib.request
     import sys
 
-# Check if a URL is provided as a command-line argument
     if len(sys.argv) < 2:
         sys.exit()
 
-# Get the URL from the command-line argument
     url = sys.argv[1]
 
     try:
-    # Use the with statement to open the URL and create a response object
         with urllib.request.urlopen(url) as response:
             if 'X-Request-Id' in response.headers:
                 x_request_id = response.headers['X-Request-Id']
