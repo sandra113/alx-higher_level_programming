0f005a16919a81c816f4ef6cca095860b7220148
root <sandramabonga303@gmail.com>
2023-12-01 16:23:29 -0800

    Response header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """this module fetches from url using the urllib package"""
-import urllib.request
-import sys
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
 
 # Check if a URL is provided as a command-line argument
-if len(sys.argv) < 2:
-    sys.exit()
+    if len(sys.argv) < 2:
+        sys.exit()
 
 # Get the URL from the command-line argument
-url = sys.argv[1]
+    url = sys.argv[1]
 
-try:
+    try:
     # Use the with statement to open the URL and create a response object
-    with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(url) as response:
+            if 'X-Request-Id' in response.headers:
+                x_request_id = response.headers['X-Request-Id']
+                print(x_request_id)
+            else:
+                print("X-Request-Id header not found in the response.")
 
-        if 'X-Request-Id' in response.headers:
-            x_request_id = response.headers['X-Request-Id']
-            print(x_request_id)
-        else:
-            print("X-Request-Id header not found in the response.")
-
-except urllib.error.URLError as e:
-    print("Error accessing the URL:", e)
+    except urllib.error.URLError as e:
+        print("Error accessing the URL:", e)
