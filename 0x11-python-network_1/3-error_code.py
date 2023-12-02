ceae3e0307d19ce3eac6cc60325c151c87b80b7a
root <sandramabonga303@gmail.com>
2023-12-01 17:21:06 -0800

    urllib

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <URL>")
+        sys.exit(1)
+
+    # Get the URL from the command-line argument
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+
+            # Print the body of the response
+            print("Body response:")
+            print(body)
+
+    except urllib.error.HTTPError as e:
+        # Handle HTTP errors and print the HTTP status code
+        print("Error code:", e.code)
+
+    except urllib.error.URLError as e:
+        print("Error accessing the URL:", e)
